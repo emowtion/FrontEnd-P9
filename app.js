4317c309a1de4460ed4285186a5b7693523d0365
emowtion <46008834+emowtion@users.noreply.github.com>
2020-10-02 18:22:43 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const options = {
+    strings: [" ",  'I\'m Emowtion A FrontEnd Developer :)'],
+    typeSpeed: 45,
+    loop: true,
+    fadeOut: true
+};
+
+const typed = new Typed(`#home h2`, options);
