07ec2bc4afcbb99eea71c1bc310a022fb33a1e80
emowtion <46008834+emowtion@users.noreply.github.com>
2020-10-03 14:49:40 +0200

    final push

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* typed libraries */
+
 const options = {
     strings: [" ", 'I\'m Emowtion, A FrontEnd Developer :)'],
     typeSpeed: 45,
@@ -6,3 +8,21 @@ const options = {
 };
 
 const typed = new Typed(`#home h2`, options);
+
+
+/* event for contact form */
+
+const $name = $('#name');
+const $email = $('#email');
+const $textArea = $('#textarea');
+const $submitBtn = $('#submit-btn');
+const $form = $('form');
+const $alertDiv = $('.alert')
+$($alertDiv).hide()
+
+$($submitBtn).on('click submit', (e) => {
+    e.preventDefault()
+    if ($($name.val() === '' && $($email.val() === ''))) {
+        $($alertDiv).show()
+    }
+});
